823c4d619f0e25f649ee82cadf42d65cfe32f8a8
pankaj <pankajkumarsingh761@gmail.com>
2019-01-07 21:20:48 +0530

    first commit

---
+++ src/main/java/com/pankaj/httpsexample/httpsexample/HttpsExampleApplication.java
@@ -0,0 +1,14 @@
+package com.pankaj.httpsexample.httpsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpsExampleApplication.class, args);
+	}
+
+}
+
